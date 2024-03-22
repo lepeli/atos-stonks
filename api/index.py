22142f0722270688822b90f3205199bd998825b3
lepeli <accounts@yvan.dev>
2024-03-22 19:23:09 +0100

    fix realtime

--- api/index.py
+++ api/index.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 @app.route("/stonks", methods=["GET", "OPTIONS"])
 def stonks():
-    r = requests.get("https://live.euronext.com/en/intraday_chart/getChartData/FR0000051732-XPAR/intraday")
+    r = requests.get("https://live.euronext.com/en/intraday_chart/getChartData/FR0000051732-XPAR/max", headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0"})
     stonks = r.json()
-    data = {"price": stonks[-1]["price"],"stonks": r.json()[-100:]}
+    r_ = requests.get("https://live.euronext.com/en/intraday_chart/getChartData/FR0000051732-XPAR/intraday", headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0"})
+    stonks_ = r_.json()
+    data = {"price": stonks_[-1]["price"],"stonks": r.json()[-1000:]}
     resp = jsonify(data)
     resp.headers.add("Access-Control-Allow-Origin", "*")
     resp.headers.add("Access-Control-Allow-Headers", "*")
